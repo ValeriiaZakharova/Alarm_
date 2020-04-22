205d852c8751f0e3ee961a2d75b72fd66a384aee
Valeriia Zakharova <zakh.valeriia@gmail.com>
2020-04-22 20:53:28 +0300

    timePicker has been added to createAlarmViewController

---
+++ Alarm/AlarmModel/AlarmModel.swift
@@ -0,0 +1,15 @@
+//
+//  AlarmModel.swift
+//  Alarm
+//
+//  Created by Valeriia Zakharova on 22.04.2020.
+//  Copyright © 2020 Valeriia Zakharova. All rights reserved.
+//
+
+import Foundation
+
+struct Alarm {
+
+    var title: String
+    
+}
--- Alarm/ViewControllers/CreateAlarmViewController.swift
+++ Alarm/ViewControllers/CreateAlarmViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 
 class CreateAlarmViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
     
+    @IBOutlet weak var timeTextField: UITextField!
+    
+    private var timePicker: UIDatePicker?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        setUpTimePicker()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        view.addGestureRecognizer(tapGesture)
     }
     
     @IBAction func cancelButtonTapped(_ sender: Any) {
@@ -28,6 +35,29 @@ class CreateAlarmViewController: UIViewController {
         
         // save a new alarm here
         
+    }
+    
+    @objc func viewTapped(gesture: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    func setUpTimePicker() {
+        
+        timePicker = UIDatePicker()
+        timePicker?.datePickerMode = .time
+        
+        timePicker?.addTarget(self, action: #selector(timeChanged(timePicker:)), for: .valueChanged)
+        
+        timeTextField.inputView = timePicker
+    }
+    
+    @objc func timeChanged(timePicker: UIDatePicker) {
+        let timeFormatter = DateFormatter()
+        timeFormatter.timeStyle = .short
+        
+        //timeFormatter.dateFormat = "HH:mm"
+        timeTextField.text = timeFormatter.string(from: timePicker.date)
+        view.endEditing(true)
     }
 
 }
