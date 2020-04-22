029111015317355eb267cc71f1d16731fff8d21a
Valeriia Zakharova <zakh.valeriia@gmail.com>
2020-04-22 17:30:44 +0300

    CreateAlarmViewController was added

---
+++ Alarm/ViewControllers/CreateAlarmViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CreateNewAlarmViewController.swift
+//  Alarm
+//
+//  Created by Valeriia Zakharova on 22.04.2020.
+//  Copyright © 2020 Valeriia Zakharova. All rights reserved.
+//
+
+import UIKit
+
+class CreateAlarmViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        
+        // save a new alarm here
+        
+    }
+
+}
